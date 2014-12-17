e3933ea13a25be868ad0edcfcf964b7bca22cc6b
Byrdann Fox <mobileupstate@gmail.com>
2014-12-16 19:35:04 -0500

    Updates...

---
+++ core.js
@@ -0,0 +1,34 @@
+(function($) {
+
+	"use strict";
+
+	var IndexView = Backbone.View.extend({
+		
+	});
+
+	var AboutView = Backbone.View.extend({
+		//
+	});
+
+	var Router = Backbone.Router.extend({
+
+		routes: {
+			'': 'index',
+			'about': 'about'
+		},
+
+		index: function () {
+
+
+
+		},
+
+		about: function () {
+
+
+
+		}
+
+	});
+
+}(jQuery));
