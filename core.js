396d16435116ef72c182417a883ffe42daa31f33
Byrdann Fox <mobileupstate@gmail.com>
2014-12-16 20:08:06 -0500

    Updates...

--- core.js
+++ core.js
@@ -1,34 +1,48 @@
 (function($) {
 
 	"use strict";
+	
+	// dep. checker...
+	console.log(jQuery);
+	console.log(_);
+	console.log(Backbone);
 
 	var IndexView = Backbone.View.extend({
-		
+		tagName: 'div',
+		el: '#spa',
+		template: _.template($('#index-view').html()),
+		render: function() {
+			$(this.el).html(this.template({}));
+		}
 	});
-
-	var AboutView = Backbone.View.extend({
-		//
+	
+	var AboutView = Backbone.View.extend({	
+		tagName: 'div',
+		el: '#spa',
+		template: _.template($('#about-view').html()),
+		render: function() {			
+			$(this.el).html(this.template({}));
+		}
 	});
-
+	
 	var Router = Backbone.Router.extend({
-
 		routes: {
 			'': 'index',
 			'about': 'about'
 		},
-
 		index: function () {
-
-
-
+			var indexView = new IndexView;
+			indexView.render();
 		},
-
 		about: function () {
-
-
-
+			var aboutView = new AboutView;
+			aboutView.render();
 		}
+	});
 
+	$(function() {
+		new Router;
+		Backbone.history.start();
 	});
 
 }(jQuery));
